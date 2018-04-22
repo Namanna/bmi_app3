841594b4c9cd8029ddc5b5800899ddc5a1872c16
Nam Hoai <chotenmay@gmail.com>
2018-04-22 15:46:17 +0700

    add somethings

--- app/src/main/java/com/example/admin/calcbmi/input.java
+++ app/src/main/java/com/example/admin/calcbmi/input.java
@@ -1,13 +1,34 @@
 package com.example.admin.calcbmi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class input extends AppCompatActivity {
+Button button;
+EditText editText, editText2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+        editText=(EditText)findViewById(R.id.editText);
+        editText2=(EditText)findViewById(R.id.editText2);
+        button=(Button)findViewById(R.id.button);
     }
+        public void btnClick(View v)
+        {
+            Intent i = new Intent();
+            String a;
+            String b;
+            a=editText.getText().toString();
+            b=editText2.getText().toString();
+           /* i.putExtra("Value", TextView);*/
+        }
+
 }
