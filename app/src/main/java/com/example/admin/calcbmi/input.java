9715d5e92eacb5ffdf698838a56241a5fd1bf940
Nam Hoai <chotenmay@gmail.com>
2018-07-17 18:27:01 +0700

    fix loi ung dung dong khi khong nhap du lieu

--- app/src/main/java/com/example/admin/calcbmi/input.java
+++ app/src/main/java/com/example/admin/calcbmi/input.java
@@ -1,5 +1,6 @@
 package com.example.admin.calcbmi;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
@@ -15,81 +16,91 @@ import java.text.DecimalFormat;
 import static android.renderscript.ScriptGroup.*;
 
 public class input extends AppCompatActivity {
-Button button;
-EditText editText, editText2;
-TextView text2, text5;
+    Button button;
+    EditText editText, editText2;
+    TextView text2, text5;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
-        editText=(EditText)findViewById(R.id.editText);
-        editText2=(EditText)findViewById(R.id.editText2);
-        button=(Button)findViewById(R.id.button);
-        text2=(TextView)findViewById(R.id.text2);
-        text5=(TextView)findViewById(R.id.text5);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        button = (Button) findViewById(R.id.button);
+        text2 = (TextView) findViewById(R.id.text2);
+        text5 = (TextView) findViewById(R.id.text5);
 
     }
-        public void btnClick(View v)
+
+    @SuppressLint("ResourceAsColor")
+    public void btnClick(View v) {
+
+        String a;
+        String b;
+        float rf, an, bn;
+        String rs;
+
+
+        if (editText.length()==0 && editText2.length()==0) {
+            Toast.makeText(this, "Hãy nhập cân nặng và chiều cao!", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (editText.length() == 0)
         {
-
-            String a;
-            String b;
-            float rf, an, bn;
-            String rs;
-
-
-
-            a  = editText.getText().toString();
+            Toast.makeText(this, "Hãy nhập số cân!", Toast.LENGTH_SHORT).show();
+        }
+        else if(editText2.length() == 0)
+        {
+            Toast.makeText(this, "Hãy nhập chiều cao!", Toast.LENGTH_SHORT).show();
+        }
+        else
+            {
+            a = editText.getText().toString();
+            b = editText2.getText().toString();
 
             an = Float.parseFloat(a);
 
 
-                b = editText2.getText().toString();
-                bn = Float.parseFloat(b);
-                if(an==0 && bn==0)
-                {
-                    Toast.makeText(this,"Giá trị không thể bằng 0!",Toast.LENGTH_SHORT).show();
+            bn = Float.parseFloat(b);
 
-                }
-                else if (an==0)
-                {
-                    Toast.makeText(this, "Số kg không thể bằng 0!",Toast.LENGTH_SHORT).show();
-                    editText.selectAll();
-                }
-                else if (bn==0)
-                {
-                    Toast.makeText(this, "Số M không thể bằng 0!",Toast.LENGTH_SHORT).show();
-                    editText2.selectAll();
-                }
-                else {
-                    rf = an / (bn * bn);
-                    /* 2 dong sau la de lam tron so*/
-                    rf = Math.round(rf * 1000) / 10;
-                    rf = rf / 100;
+            if (an == 0 && bn == 0) {
+                Toast.makeText(this, "Giá trị không thể bằng 0!", Toast.LENGTH_SHORT).show();
 
-                    rs = Float.toString(rf);
-                    text2.setText(rs);
+            } else if (an == 0) {
+                Toast.makeText(this, "Số kg không thể bằng 0!", Toast.LENGTH_SHORT).show();
+                editText.selectAll();
 
-                    if (rf < 18.5) {
-                        text5.setText("Bạn quá gầy!");
-                    } else if (18.5 <= rf && rf <= 22.9) {
-                        text5.setText("Ok, ko gầy cũng chẳng béo");
-                    } else if (23 <= rf && rf <= 24.9) {
-                        text5.setText("Hơi hơi béo rồi");
-                    } else if (25 <= rf && rf <= 29.9) {
-                        text5.setText("Béo phì!");
-                    } else if (30 <= rf && rf <= 39.9) {
-                        text5.setText("Béo lắm rồi! Ăn ít thôi!");
-                    } else if (rf >= 40) {
-                        text5.setText("Kiểm tra xem dữ liệu nhập vào có chính xác không, nếu chính xác, chúc mừng! Bạn là FAT MAN đích thực!");
-                    } else {
-                        text5.setText("Lỗi! Xin hãy thử lại!");
-                    }
+            } else if (bn == 0) {
+                Toast.makeText(this, "Số M không thể bằng 0!", Toast.LENGTH_SHORT).show();
+                editText2.selectAll();
+            } else {
+                rf = an / (bn * bn);
+                /* 2 dong sau la de lam tron so*/
+                rf = Math.round(rf * 1000) / 10;
+                rf = rf / 100;
 
+                rs = Float.toString(rf);
+                text2.setText(rs);
+
+                if (rf < 18.5) {
+                    text5.setText("Bạn quá gầy!");
+                } else if (18.5 <= rf && rf <= 22.9) {
+                    text5.setText("Ok, ko gầy cũng chẳng béo");
+                } else if (23 <= rf && rf <= 24.9) {
+                    text5.setText("Hơi hơi béo rồi");
+                } else if (25 <= rf && rf <= 29.9) {
+                    text5.setText("Béo phì!");
+                } else if (30 <= rf && rf <= 39.9) {
+                    text5.setText("Béo lắm rồi! Ăn ít thôi!");
+                } else if (rf >= 40) {
+                    text5.setText("Kiểm tra xem dữ liệu nhập vào có chính xác không, nếu chính xác, chúc mừng! Bạn là FAT MAN đích thực!");
+                } else {
+                    text5.setText("Lỗi! Xin hãy thử lại!");
                 }
+
             }
         }
+    }
 
-
+}
